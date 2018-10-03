09a6e7d338788b54fb29ec13495c5c480c476e8a
Oleg <super.oleg.3000@yandex.ru>
2018-10-03 16:29:36 +0300

    Add trending repos getter, printer

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,38 @@
+import sys
+from datetime import date, timedelta
+
+import requests
 
 
-def get_trending_repositories(top_size):
-    pass
+def get_trending_repositories(max_repos):
+    api_url = "https://api.github.com/search/repositories"
+    headers = {"Accept": "application/vnd.github.v3+json"}
+    week_ago = date.today() - timedelta(weeks=1)
+    params = {
+        "q": "created:>={}".format(week_ago.isoformat()),
+        "sort": "stars",
+        "per_page": max_repos,
+    }
+    response = requests.get(api_url, params=params, headers=headers)
+    repos = response.json()["items"]
+    return repos
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def print_repos(repos):
+    for repo in repos:
+        print(
+            "{}: {} stars, {} issue(s) -> {}".format(
+                repo["name"],
+                repo["stargazers_count"],
+                repo["open_issues_count"],
+                repo["html_url"],
+            )
+        )
 
-if __name__ == '__main__':
-    pass
+
+if __name__ == "__main__":
+    try:
+        trending_repos = get_trending_repositories(max_repos=20)
+        print_repos(trending_repos)
+    except requests.RequestException as error:
+        sys.exit(error)
