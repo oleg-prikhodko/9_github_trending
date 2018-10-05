0e25cc7426622b7ea97147bb52f89fefde325573
Oleg <super.oleg.3000@yandex.ru>
2018-10-05 11:03:11 +0300

    Add fixes for assignment

--- github_trending.py
+++ github_trending.py
@@ -1,17 +1,7 @@
 import sys
 from datetime import date, timedelta
-from functools import reduce
 
 import requests
-
-
-def validate_response(response):
-    ok_status_code = 200
-    if response.status_code != ok_status_code:
-        message = "Server responded with status code {}\n{}".format(
-            response.status_code, response.json().get("message")
-        )
-        raise ValueError(message)
 
 
 def get_trending_repositories(max_repos, since):
@@ -23,7 +13,7 @@ def get_trending_repositories(max_repos, since):
         "per_page": max_repos,
     }
     response = requests.get(api_url, params=params, headers=headers)
-    validate_response(response)
+    response.raise_for_status()
     repos = response.json()["items"]
     return repos
 
@@ -34,16 +24,11 @@ def get_open_issues_amount(repo_owner, repo_name):
     )
     params = {"state": "open"}
     response = requests.get(api_url, params=params)
-    validate_response(response)
+    response.raise_for_status()
 
     issues = response.json()
-    accumulator_initial_value = 0
-    issues_amount = reduce(
-        lambda accumulator, issue: accumulator + 1
-        if "pull_request" not in issue
-        else accumulator,
-        issues,
-        accumulator_initial_value,
+    issues_amount = len(
+        [issue for issue in issues if "pull_request" not in issue]
     )
     return issues_amount
 
@@ -72,5 +57,5 @@ if __name__ == "__main__":
         ]
         repos_with_issues_amount = zip(trending_repos, issues_amounts)
         print_repos(repos_with_issues_amount)
-    except (ValueError, requests.RequestException) as error:
+    except requests.RequestException as error:
         sys.exit(error)
